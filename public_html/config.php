2283c5433ca8dc16871d39547253dd96c88002fe
Leon200211 <leon200207@yandex.ru>
2023-01-14 17:20:05 +0300

    Добавил проверку заголовок и кода ответа

--- public_html/config.php
+++ public_html/config.php
@@ -5,7 +5,9 @@ defined('VG_ACCESS') or die('Access denied');
 
 
 // константы
-const SITE_URL = 'http://fitlent.online.store';
+//const SITE_URL = 'http://fitlent.online.store';
+const SITE_URL = 'https://ya.ru/';
+
 const PATH = '/';
 
 const HOST = 'localhost';
--- public_html/core/admin/controllers/CreatesitemapController.php
+++ public_html/core/admin/controllers/CreatesitemapController.php
@@ -4,12 +4,13 @@
 namespace core\admin\controllers;
 
 
+
 use core\base\controllers\BaseMethods;
 
 
 
 // контроллер для создания карты сайта
-class CreatesitemapController
+class CreatesitemapController extends BaseAdmin
 {
 
     use BaseMethods;
@@ -28,9 +29,9 @@ class CreatesitemapController
     protected function inputData(){
 
         // если у нас не установлена библиотека curl
-        if(function_exists('curl_init')){
+        if(!function_exists('curl_init')){
             $this->writeLog('Отсутствует библиотека CURL');
-            $_SESSION['res']['answer'] = '<div class="error">Library CURL as apsent. Creation of sitemap imposible!</div>';
+            $_SESSION['res']['answer'] = '<div class="error">Library CURL as absent. Creation of sitemap impossible!</div>';
             $this->redirect();
         }
 
@@ -60,8 +61,59 @@ class CreatesitemapController
     }
 
 
+
     // метод для парсинга сайта
     protected function  parsing($url, $index = 0){
+
+
+        // проверяем на конечный слеш в адресе сайта
+        if(mb_strlen(SITE_URL) + 1 === mb_strlen($url) and mb_strrpos($url, '/') === mb_strlen($url) - 1){
+            return;
+        }
+
+
+        // инициализируем CURL
+        $curl = curl_init();  // дескриптор
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);  // получаем ответы
+        curl_setopt($curl, CURLOPT_HEADER, true);  // ответы заголовков
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);  // следовать ли за редиректами
+        curl_setopt($curl, CURLOPT_TIMEOUT, 120);  // время ожидания от сервера
+        curl_setopt($curl, CURLOPT_RANGE, 0 - 4194304);  // ограничиваем объем данных на загрузку ( 4 Mb)
+
+
+        // отправка CURL запроса
+        $out = curl_exec($curl);
+
+        // уничтожаем дескриптор
+        curl_close($curl);
+
+
+        // ищем заголовки
+        if(!preg_match('/Content-Type:\s+text\/html/uis', $out)){
+
+            unset($this->linkArr[$index]);
+            $this->linkArr = array_values($this->linkArr);
+
+            return;
+
+        }
+
+
+        // проверяем код ответа
+        if(!preg_match('/HTTP\/\d\.?\d?\s+20\d/uis', $out)){
+
+            $this->writeLog('Не корректная сслыка при парсинге - ' . $url, $this->parsingLogFile);
+
+            unset($this->linkArr[$index]);
+            $this->linkArr = array_values($this->linkArr);
+
+            $_SESSION['res']['answer'] = '<div class="error">Incorrect link in parsing - ' . $url . '<br>Sitemap is created' . '</div>';
+
+            return;
+
+        }
 
 
 
